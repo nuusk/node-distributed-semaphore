1382321ee2b9b81a9d06ae847c2e2e5e164f2a41
Poe <piotr.a.ptak@icloud.com>
2019-10-07 09:16:50 +0200

    Apply port and host from env variables

--- server.js
+++ server.js
@@ -3,21 +3,24 @@ const WebSocketServer = require('rpc-websockets').Server;
 const debug = require('debug')('server');
 const Semaphore = require('./services/Semaphore');
 
-const { DEBUG_ENABLED, SEMAPHORE_CAPACITY } = process.env;
+const {
+  DEBUG_ENABLED, SEMAPHORE_CAPACITY, PORT, HOST,
+} = process.env;
 
 debug.enabled = DEBUG_ENABLED;
 
 const semaphoreCapacity = SEMAPHORE_CAPACITY || 20;
+const port = PORT || 8080;
+const host = HOST || 'localhost';
 
 debug('Attempting to create a semaphore.');
 const semaphore = new Semaphore(semaphoreCapacity).then((message) => {
   debug(message);
 });
 
-const server = new WebSocketServer({
-  port: 8080,
-  host: 'localhost',
-});
+
+const server = new WebSocketServer({ port, host });
+debug(`Server started on ${host}:${port}`);
 
 // register an RPC method
 server.register('sum', (params) => params[0] + params[1]);
